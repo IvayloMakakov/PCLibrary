0a0b35b497f6ce11f200c1a32f1699b35a4ce99a
IvayloMakakov <ivailo_makakov@abv.bg>
2022-03-15 21:01:55 +0200

    Ivo changes Context

--- LibrarySoftware/Business/LibraryBusiness.cs
+++ LibrarySoftware/Business/LibraryBusiness.cs
@@ -36,5 +36,57 @@ namespace Business
         {
             return libraryContext.LibraryCards.Find(id);
         }
+
+        public void AddBook(Book book)
+        {
+            libraryContext.Books.Add(book);
+            libraryContext.SaveChanges();
+        }
+
+        public void UpdateBook(Book book)
+        {
+            Book updatedBook = libraryContext.Books.Find(book.BookId);
+            if (updatedBook != null)
+            {
+                libraryContext.Entry(updatedBook).CurrentValues.SetValues(book);
+                libraryContext.SaveChanges();
+            }
+        }
+
+        public void DeleteBook(int id)
+        {
+            Book product = libraryContext.Books.Find(id);
+            if (product != null)
+            {
+                libraryContext.Books.Remove(product);
+                libraryContext.SaveChanges();
+            }
+        }
+
+        public void AddCard(LibraryCard card)
+        {
+            libraryContext.LibraryCards.Add(card);
+            libraryContext.SaveChanges();
+        }
+
+        public void UpdateCard(LibraryCard card)
+        {
+            LibraryCard updatedCard = libraryContext.LibraryCards.Find(card.Id);
+            if (updatedCard != null)
+            {
+                libraryContext.Entry(updatedCard).CurrentValues.SetValues(card);
+                libraryContext.SaveChanges();
+            }
+        }
+
+        public void DeleteCard(int id)
+        {
+            LibraryCard card = libraryContext.LibraryCards.Find(id);
+            if (card != null)
+            {
+                libraryContext.LibraryCards.Remove(card);
+                libraryContext.SaveChanges();
+            }
+        }
     }
 }
