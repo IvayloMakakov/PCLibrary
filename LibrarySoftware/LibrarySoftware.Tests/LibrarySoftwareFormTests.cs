3b7f2173a977a692368c733dbd7f1b78334d8691
IvayloMakakov <ivailo_makakov@abv.bg>
2022-03-19 14:34:27 +0200

    Unit tests

---
+++ LibrarySoftware/LibrarySoftware.Tests/LibrarySoftwareFormTests.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace LibrarySoftware.Tests
+{
+    [TestFixture]
+    public class LibrarySoftwareFormTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
