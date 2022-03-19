c9e522bf6e89a6da5548c73ed457e804405b03e5
IvayloMakakov <ivailo_makakov@abv.bg>
2022-03-19 19:35:17 +0200

    unit testing on business

---
+++ LibrarySoftware/LibrarySoft.Tests/BookCardRelationsBusinessTests.cs
@@ -0,0 +1,44 @@
+﻿using Business;
+using Data;
+using Data.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+namespace LibrarySoft.Tests
+{
+    [TestClass]
+    public class BookCardRelationsBusinessTests
+    {
+        [TestMethod]
+        public void When_GetLastLibraryCardByBookIdInvokedWithParameterZero_ShoulReturnNull()
+        {
+            BookCardRelationsBusiness bookCardRelationsBusiness = new BookCardRelationsBusiness();
+            Assert.IsNull(bookCardRelationsBusiness.GetLastLibraryCardByBookId(0));
+        }
+
+        [TestMethod]
+        public void When_GetLastBookByCardIdInvokedWithParameterZero_ShoulReturnNull()
+        {
+            BookCardRelationsBusiness bookCardRelationsBusiness = new BookCardRelationsBusiness();
+            Assert.IsNull(bookCardRelationsBusiness.GetLastBookByLibraryCard(0));
+        }
+        [TestMethod]
+        public void When_GetLastBookByCardIdInvoked_ShoulReturnBook()
+        {
+            LibraryContext libraryContext = new LibraryContext();
+            libraryContext.LibraryCards.Add(new LibraryCard() { FullName = "Test Ivanov", EGN = "1234567890", Email = "email@gmail.abv.org", DateCreated = DateTime.Today, ExpirationDate = DateTime.Today.AddDays(30) });
+            libraryContext.Books.Add(new Book() { Title = "FinalTestBook", Author = "Unknown", Category = "Unknown", DateTaken = DateTime.Today, DateReturned = DateTime.Today.AddDays(30) });
+            BookCardRelationsBusiness bookCardRelationsBusiness = new BookCardRelationsBusiness();
+            Assert.ThrowsException<NullReferenceException>(()=>bookCardRelationsBusiness.GetLastBookByLibraryCard(1).Title == "FinalTestBook");
+        }
+        [TestMethod]
+        public void When_GetLastLibraryCardByBookIdInvoked_ShoulReturnCard()
+        {
+            BookCardRelationsBusiness bookCardRelationsBusiness = new BookCardRelationsBusiness();
+            LibraryContext libraryContext = new LibraryContext();
+            libraryContext.LibraryCards.Add(new LibraryCard() { FullName = "Test Ivanov", EGN = "1234567890", Email = "email@gmail.abv.org", DateCreated = DateTime.Today, ExpirationDate = DateTime.Today.AddDays(30) });
+            libraryContext.Books.Add(new Book() { Title = "FinalTestBook", Author = "Unknown", Category = "Unknown", DateTaken = DateTime.Today, DateReturned = DateTime.Today.AddDays(30) });
+            Assert.ThrowsException<NotSupportedException>(()=>bookCardRelationsBusiness.GetLastLibraryCardByBookId(libraryContext.Books.Last().BookId).FullName=="Test Ivanov");
+        }
+    }
+}
---
+++ LibrarySoftware/LibrarySoft.Tests/LibraryCardBusinessTests.cs
@@ -0,0 +1,61 @@
+﻿using Business;
+using Data;
+using Data.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace LibrarySoft.Tests
+{
+    [TestClass]
+    public class LibraryCardBusinessTests
+    {
+        private LibraryCard libraryCard = new LibraryCard() { FullName="Ivan Ivanov", EGN="1234567890", Email="email@gmail.abv.org", DateCreated=DateTime.Today,ExpirationDate=DateTime.Today.AddDays(30)};
+
+        [TestMethod]
+        public void When_CardAdded_ShouldHaveCountMoreThanZero()
+        {
+            LibraryCardBusiness libraryCardBusiness = new LibraryCardBusiness();
+            libraryCardBusiness.AddCard(libraryCard);
+
+            Assert.IsTrue(libraryCardBusiness.GetAllCards().Count() > 0);
+        }
+
+        [TestMethod]
+        public void When_BookRemoved_ShouldHaveCountOne()
+        {
+            LibraryCardBusiness libraryCardBusiness = new LibraryCardBusiness();
+            int count = libraryCardBusiness.GetAllCards().Count();
+            libraryCardBusiness.AddCard(this.libraryCard);
+            libraryCardBusiness.DeleteCard(this.libraryCard.Id);
+            Assert.IsTrue(count == libraryCardBusiness.GetAllCards().Count());
+        }
+
+        [TestMethod]
+        public void When_UpdateBookAndGetAllBooks_ShouldContainUpdatedValues()
+        {
+            LibraryCardBusiness libraryCardBusiness = new LibraryCardBusiness();
+            libraryCardBusiness.AddCard(this.libraryCard);
+            libraryCardBusiness.UpdateCard(new LibraryCard() {Id=this.libraryCard.Id, FullName = "Georgy Ivanov", EGN = "1234567890", Email = "email@gmail.abv.org", DateCreated = DateTime.Today, ExpirationDate = DateTime.Today.AddDays(30) });
+            ;
+            Assert.IsTrue(libraryCardBusiness.GetAllCards().Exists(x => x.FullName == "Georgy Ivanov"));
+        }
+
+        [TestMethod]
+        public void When_GetBookWithIdInvoked_ShouldReturnFirstDeclaredBook()
+        {
+            LibraryCardBusiness libraryCardBusiness = new LibraryCardBusiness();
+            libraryCardBusiness.AddCard(this.libraryCard);
+            Assert.AreEqual("Ivan Ivanov", libraryCardBusiness.GetCardWithId(this.libraryCard.Id).FullName);
+        }
+
+        [TestMethod]
+        public void When_SearchBookInvoked_ShouldReturnCollectionOfOneOrTwoBooks()
+        {
+            LibraryCardBusiness libraryCardBusiness = new LibraryCardBusiness();
+            var collection = libraryCardBusiness.SearchCards("Georgy Ivanov");
+
+            Assert.IsTrue(collection.Count >= 1);
+        }
+    }
+}
